3ca1b9746b10aacee50bfe5d5e78ee6d6eadb4e1
Valiyev Heydar <heydarmv@code.local>
2023-12-24 13:58:18 +0400

    feat:complet class 4

---
+++ 04.function/script.js
@@ -0,0 +1,103 @@
+//Class Tasks
+//1
+// let num = 17
+// num > 22
+//   ? console.log('1')
+//   : console.log('2');
+
+// //2
+// let user ='Heyder';
+// user.startsWith('a')
+// ? console.log('1')
+// :console.log('2');
+// //3
+// user.endsWith('t')
+// ? console.log('1')
+// :console.log('2');
+// //4
+// function sumNum() {
+//     let a = 8
+//     let b = 9
+//     console.log(a+b);
+// }
+// sumNum(0)
+// function sumNum1() {
+//         return 8 + 9
+//     }
+//     console.log(sumNum1());
+// //5
+// function multNum() {
+//     let a = 19
+//     let b = 59
+//     let c = 9
+//     console.log(a*b*c);
+// }
+// multNum(0)
+
+// function multNum1() {
+//     return 19 * 59 * 9
+// }
+// console.log(multNum1());
+
+// //6
+// const arrNum = [6,2,6,7,2,1,88]
+// function getAvg(anyArray) {
+//     let sum = 0
+//     anyArray.map((arrNum) => sum += arrNum)
+//     return Math.round(sum / anyArray.length)
+// }
+// console.log(getAvg(arrNum));
+
+// //7
+// const studentarr = ['heyder', 'rafiq', 'matin']
+// function getusers () {
+//     const result = studentarr.filter(name => name.startsWith('r'));
+//     return result;
+//     }
+//     console.log(getusers(studentarr));
+
+// //8
+
+// function getusers1 () {
+//     const result1 = studentarr.filter(name => name.length >= 6);
+//     return result1;
+//     }
+//     console.log(getusers1(studentarr));
+
+
+//
+// for (let index = 0; index < 21; index++) {
+//     console.log(index);
+    
+// }
+
+// let myName = 'Heyder'
+// for (let i  = 0; i < myName.length; i++) {
+//     console.log(myName[i]);
+    
+// }
+
+//
+// for (let index = 2; index < 21; index+= 2) {
+//         console.log(index);
+// }
+
+//
+// for (let index = 1; index < 21; index+= 2) {
+//     console.log(index);
+// }
+
+//
+// for (let index = 0; index < 21; index++) {
+//     if (index % 2 == 0)
+//     console.log(index);
+
+// }
+
+// 
+// for (let index = 0; index < 21; index++) {
+//     if (index % 2 != 0)
+//     console.log(index);
+
+// }
+
